09137069be9797bb0815470a73bb485d90492015
abrahamiant <44582517+abrahamiant@users.noreply.github.com>
2023-06-22 15:37:58 +0400

    Initial commit

---
+++ choose_movie.py
@@ -0,0 +1,33 @@
+import os
+import random
+import requests
+import webbrowser
+
+from bs4 import BeautifulSoup
+from lxml import etree
+
+
+with open("movies.json") as f:
+    movies = json.load(f)
+
+movie = random.choice(movies)
+
+text = f'Seems like we are going to watch {movie} tonight'
+
+q = f'smotret {movie} gidonline'
+
+url = "https://www.google.com/search?q"
+
+data = requests.get(f'{url}={q}').text
+
+soup = BeautifulSoup(data, 'html.parser')
+
+dom = etree.HTML(str(soup))
+
+os.system(f"say {text}")
+
+url = dom.xpath('//*[@id="main"]/div[3]/div/div[1]/a')[0].get('href')
+
+url = f'https://www.google.com{url}'
+
+webbrowser.open(url)
